7edcf43c980787c06b9740f11b1ad020b5f14ec9
Vishalreddy2020 <145963490+Vishalreddy2020@users.noreply.github.com>
2024-10-10 21:37:12 -0400

    Update task2.sh

--- task2.sh
+++ task2.sh
@@ -1,1 +1,3 @@
-grep -rl "sample" * | xargs grep -o "CSC510" | uniq -c | awk '$1 >= 3' | sort -k1,1nr | sed 's/file_/filtered_/g'
+#!/usr/bin/env bash
+
+grep -rl "sample" dataset1/* | xargs grep -o "CSC510" | uniq -c | grep '^\s*[3-9]' | cut -d: -f1 | gawk ' { printf $0; system("ls -l " $2) } ' | gawk ' { print $1, $6, $10 } ' | sort -nr -k1,1 -k2,2 | sed 's/file_/filtered_/g'
